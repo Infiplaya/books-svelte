b9c1779fab7b82ce4a53900691dad15912acd745
Pawel Sobaniec <infipablosoban@gmail.com>
2023-03-21 19:55:41 +0100

    add shadow

--- src/routes/BookCard.svelte
+++ src/routes/BookCard.svelte
@@ -2,9 +2,21 @@
 import { enhance } from '$app/forms';
 import type { Book } from '@prisma/client';
 import type { PageData } from "./$types";
+import FaRegBookmark from 'svelte-icons/fa/FaRegBookmark.svelte'
+import FaBookmark from 'svelte-icons/fa/FaBookmark.svelte'
+import FaPlusCircle from 'svelte-icons/fa/FaPlusCircle.svelte'
+import FaCheckCircle from 'svelte-icons/fa/FaCheckCircle.svelte'
 export let book: Book;
 export let userLists: PageData['userLists'];
 export let isDetailPage: boolean;
+
+function truncateString(str: string, maxLength: number = 250) {
+  if (str.length > maxLength) {
+    return str.slice(0, maxLength - 3) + '...';
+  } else {
+    return str;
+  }
+}
 </script>
 
 	<div class="book-card">
@@ -18,37 +30,39 @@ export let isDetailPage: boolean;
 		</div>
 		{#if isDetailPage}
 		<p class="description">{book.description}</p>
+		{:else}
+		<p class="description">{truncateString(book.description)}</p>
 		{/if}
         </a>
 
 		<div class="action-buttons">
-		{#if userLists?.finishedList.map((item) => item.id).includes(book.id)}
-			<form action="?/removeFromFinishedList" method="POST" use:enhance>
-				<input type="hidden" value={book.id} id="id" name="id" />
-				<button type="submit">
-					Mark as unfinished
-				</button>
-			</form>
-		{:else}
-			<form action="?/addToFinishedList" method="POST" use:enhance>
-				<input type="hidden" value={book.id} id="id" name="id" />
-				<button type="submit">
-					Mark as finished
-				</button>
-			</form>
-		{/if}
 		{#if userLists?.readingList.map((item) => item.id).includes(book.id)}
 			<form action="?/removeFromReadingList" method="POST" use:enhance>
 				<input type="hidden" value={book.id} id="id" name="id" />
-				<button type="submit">
-					Remove from reading list
+				<button class="icon" type="submit">
+					<FaRegBookmark/>
 				</button>
 			</form>
 		{:else}
 			<form action="?/addToReadingList" method="POST" use:enhance>
 				<input type="hidden" value={book.id} id="id" name="id" />
-				<button type="submit">
-					Add to reading list
+					<button class="icon" type="submit">
+						<FaBookmark />
+					</button>
+			</form>
+		{/if}
+		{#if userLists?.finishedList.map((item) => item.id).includes(book.id)}
+			<form action="?/removeFromFinishedList" method="POST" use:enhance>
+				<input type="hidden" value={book.id} id="id" name="id" />
+				<button class="icon" type="submit">
+					<FaPlusCircle />
+				</button>
+			</form>
+		{:else}
+			<form action="?/addToFinishedList" method="POST" use:enhance>
+				<input type="hidden" value={book.id} id="id" name="id" />
+				<button class="icon" type="submit">
+					<FaCheckCircle />
 				</button>
 			</form>
 		{/if}
@@ -57,8 +71,23 @@ export let isDetailPage: boolean;
 	</div>
 
 <style>
+
+.icon {
+	background-color: transparent;
+	border: none;
+    width: 36px;
+    height: 36px;
+	cursor: pointer;
+	-webkit-font-smoothing: antialiased;
+  	-moz-osx-font-smoothing: grayscale;
+  }
+
+  .icon:hover {
+	transform: scale(1.2);
+  }
+
     .book-card {
-		background-color: aquamarine;
+		box-shadow: 0px 0px 10px rgb(119, 119, 119);
 		padding: 40px;
 		border-radius: 20px;
 		width: 400px;
@@ -93,9 +122,9 @@ export let isDetailPage: boolean;
 	}
 
 	.action-buttons {
-		margin-top: 20px;
+		align-self: flex-end;
+		margin-top: 10px;
 		display: flex;
-		flex-direction: column;
 		gap: 10px;
 	}
 

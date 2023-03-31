f61778cab17223aea5053c72b0a188d19817bf3d
Pawel Sobaniec <infipablosoban@gmail.com>
2023-03-31 14:32:13 +0200

    test edge

--- src/lib/components/Hamburger.svelte
+++ src/lib/components/Hamburger.svelte
@@ -4,16 +4,16 @@
 		open = !open;
 	};
 	export let ariaLabel = 'toggle menu';
-	export let width: string | number = 80;
+	export let width: string | number = 60;
 </script>
 
 <button on:click={onClick} aria-expanded={open} aria-label={ariaLabel} class="hamburger-button">
-	<svg class:open viewBox="0 0 100 100" fill="none" stroke="currentColor" stroke-width="5" {width}>
+	<svg class:open viewBox="0 0 100 100" fill="none" stroke="currentColor" stroke-width="4" {width}>
 		<path
 			class="top"
 			d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
 		/>
-		<path class="middle" d="m 30,50 h 40" />
+		<path class="middle" d="m 30,50 h 60" />
 		<path
 			class="bottom"
 			d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"

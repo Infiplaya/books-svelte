57fea126040df5d264ed3a2af9d8b956c68b3373
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-01 16:25:05 +0200

    remove bypass token

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import { fail, type Actions, type ServerLoad } from '@sveltejs/kit';
 import { prisma } from '../lib/server/prisma';
 
-import { BYPASS_TOKEN } from '$env/static/private';
 
 export const config = {
 	isr: {
@@ -12,8 +11,6 @@ export const config = {
 		// Random token that can be provided in the URL to bypass the cached version of the asset, by requesting the asset
 		// with a __prerender_bypass=<token> cookie.
 		//
-		// Making a `GET` or `HEAD` request with `x-prerender-revalidate: <token>` will force the asset to be re-validated.
-		bypassToken: BYPASS_TOKEN,
 	}
 };
 
